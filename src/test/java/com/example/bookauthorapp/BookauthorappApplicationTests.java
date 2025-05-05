974b109cbf5b1f4d2630fe0636483bb9466eaa39
Aditi Singh <aditi4072005@gmail.com>
2025-05-05 20:56:56 +0530

    adding files

---
+++ src/test/java/com/example/bookauthorapp/BookauthorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookauthorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookauthorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
